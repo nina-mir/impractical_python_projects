795aa5f68fded4578aa26c876b27a2eaea68664f
nina-mir <ninamirf@gmail.com>
2020-02-23 07:13:25 -0800

    pylint corrected

--- silly_name_generator/silly.py
+++ silly_name_generator/silly.py
@@ -1,21 +1,36 @@
-import sys, random
+"""
+Generates funny first and last names from 2 sepearte lists randomly.
+1st project of the book of Impractical Python Projects.
 
-print('Welcome to the name picker project!')
+"""
 
-first = ("Nina 'Bonina'", 'Goat Jackson', "Tina 'O-Nina'", 'Cadillac Joe',
-        'Shampoo Doug', 'Treebird')
-last = ('Torabi', 'Schlect','Richtig', 'dortdorf', 'Nottoday',
+import sys
+import random
+
+def main():
+    """
+    Choose a name at random from two lists to print to screen in a loop
+    untill the user quits.
+    """
+
+    print('Welcome to the name picker project!')
+    first = ("Nina 'Bonina'", 'Goat Jackson', "Tina 'O-Nina'", 'Cadillac Joe',
+             'Shampoo Doug', 'Treebird')
+    last = ('Torabi', 'Schlecht', 'Richtig', 'dortdorf', 'Nottoday',
             'MaybeLater')
 
-while True:
-    firstName = random.choice(first)
-    lastName = random.choice(last)
-    print("\n\n")
-    print("{} {}".format(firstName, lastName), file=sys.stderr)
-    print("\n\n")
+    while True:
+        first_name = random.choice(first)
+        last_name = random.choice(last)
+        print("\n\n")
+        print("{} {}".format(first_name, last_name), file=sys.stderr)
+        print("\n\n")
 
-    try_again = input('Try again? Press Enter or n to quit. ')
-    if try_again.lower() == 'n':
-        break
+        try_again = input('Try again? Press Enter or n to quit. ')
+        if try_again.lower() == 'n':
+            break
 
-input("press Enter to exit! ")
+    input("press Enter to exit! ")
+
+if __name__ == "__main__":
+    main()
